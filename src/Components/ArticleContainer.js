0070faf6b735587f0e37ac232e96ff5ab66743c5
Joshua Wu <jww2145@columbia.edu>
2022-07-13 15:44:03 -0400

    changed google charts

--- src/Components/ArticleContainer.js
+++ src/Components/ArticleContainer.js
@@ -10,13 +10,67 @@ function ArticleContainer({summary, whichDisplay, data}) {
 
   const displayChart = whichDisplay === 'ner'
 
+  console.log(data)
+
+  let top = []
+
+  if(displayChart){
+    top.push(data[0])
+      
+    let newArray = data.slice(1)
+
+    let sum = 0
+
+    newArray.forEach(arr => {
+        sum += arr[1]
+    })
+
+    let average = sum / (data.length - 1)
+    let others = 0
+
+    data.forEach(arr => {
+      if(arr[1] > average){
+        top.push(arr)
+      }else{
+        others++
+      }
+    })
+
+    top.push(['Others', others])
+  }
+
+  const options = {
+    'width': 770,
+    'height': 400,
+    chartArea:{
+      width: '100%',
+      height: '80%',
+    },
+    colors: ['#AF9f8C', '#7F867B', '#C7C7BB', '#D4C8BE', 'D4BAB0', '#7F867B', '#C1ABAD', '#C7C7BB', '#BA494B', '#E5CBC1', '#F0E7DA', '#A6B4B3', '#E3E4E0'],
+    is3D: true,
+    backgroundColor: {
+        fill: '#EFEDE7',
+        stroke: '#b0b0b0',
+        strokeWidth: 2
+      },
+    legend: { position: "bottom" }
+  };
+
+
   return (
     <div>
-        {!displayChart && showSummary}
-        {displayChart && <Chart 
-          chartType = "PieChart"
-          data = {data}
-        />}
+      {!displayChart && <div id='summary-container'>
+        <h2>{displayChart ? 'Named Entities' : 'Summary'}</h2>
+          {showSummary}
+      </div>}
+      {displayChart && <div id = "chart-container">
+        <h2>{displayChart ? 'Named Entities' : 'Summary'}</h2>
+        {<Chart 
+            chartType = "PieChart"
+            data = {top}
+            options = {options}
+          />}
+      </div>}
     </div>
   )
 }
